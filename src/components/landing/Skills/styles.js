32349439abfd388281d5e8b70d399d62f8dd5169
Arthur Puyou <arthur@puyou.fr>
2019-06-26 19:29:18 +0200

    Align skills to top

--- src/components/landing/Skills/styles.js
+++ src/components/landing/Skills/styles.js
@@ -10,7 +10,7 @@ export const Wrapper = styled.div`
 export const SkillsWrapper = styled.div`
   padding: 4rem 0;
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: space-between;
 
   @media (max-width: 960px) {
