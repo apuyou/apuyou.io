5fc17f6d58d04ce60a922d0747dda2c98bbe8172
Arthur Puyou <arthur@puyou.fr>
2019-06-28 01:31:36 +0200

    Add Amplitude

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Layout, SEO } from '../components/common';
 import { Intro, Skills, Contact, Projects } from '../components/landing';
 
-export default () => (
-  <Layout>
-    <SEO />
-    <Intro />
-    <Projects />
-    <Skills />
-    <Contact />
-  </Layout>
-);
+export default () => {
+  useEffect(() => {
+    import('amplitude-js').then(amplitude => {
+      amplitude.getInstance().init('65f3ee7b7b7d7190d21c96c3c84ba07b');
+      amplitude.getInstance().logEvent('View homepage');
+    });
+  }, []);
+
+  return (
+    <Layout>
+      <SEO />
+      <Intro />
+      <Projects />
+      <Skills />
+      <Contact />
+    </Layout>
+  );
+};
