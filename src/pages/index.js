fe43608033a6e220e0734336c83a3a8d4988571d
Arthur Puyou <arthur@puyou.fr>
2019-08-30 14:26:02 +0200

    Replace Amplitude with Segment

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,7 @@ import { Intro, Skills, Contact, Projects } from '../components/landing';
 
 export default () => {
   useEffect(() => {
-    import('amplitude-js').then(amplitude => {
-      amplitude.getInstance().init('65f3ee7b7b7d7190d21c96c3c84ba07b', null, {
-        includeUtm: true,
-        includeReferrer: true,
-      });
-      amplitude.getInstance().logEvent('View homepage');
-    });
+    window.analytics.track('View homepage');
   }, []);
 
   return (
