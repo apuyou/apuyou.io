f2e9907cc3764be063cabb5bbdbb5b536e9abcf2
Arthur Puyou <arthur@puyou.fr>
2020-01-09 17:28:14 +0100

    Send device id to analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,16 @@ import React, { useEffect } from 'react';
 import { Layout, SEO } from '../components/common';
 import { Intro, Skills, Contact, Projects } from '../components/landing';
 import axios from 'axios';
+import uuid from 'uuid/v4';
 
 export default () => {
   useEffect(() => {
-    axios.get('/.netlify/functions/view');
+    let deviceId = localStorage.getItem('device-id');
+    if (!deviceId) {
+      deviceId = uuid();
+      localStorage.setItem('device-id', deviceId);
+    }
+    axios.post('/.netlify/functions/view', { device_id: deviceId });
   }, []);
 
   return (
