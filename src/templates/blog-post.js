ca290d0cfd7eb5f66b993ffde4b7f9bff15318cb
Arthur Puyou <arthur@puyou.fr>
2020-07-26 15:05:46 +0200

    Remove unused classes from posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -15,14 +15,11 @@ export default function Template({ data }) {
       <SEO />
       <Header />
       <Container>
-        <div className="blog-post-container">
+        <div>
           <Helmet title={`${post.frontmatter.title} - ${legalName}`} />
           <div className="blog-post">
             <h1>{post.frontmatter.title}</h1>
-            <div
-              className="blog-post-content"
-              dangerouslySetInnerHTML={{ __html: post.html }}
-            />
+            <div dangerouslySetInnerHTML={{ __html: post.html }} />
           </div>
         </div>
       </Container>
