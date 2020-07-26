67bcfb6b15d3191104b4c4abc473d87097fe8c7f
Arthur Puyou <arthur@puyou.fr>
2020-07-26 15:11:40 +0200

    Do not build draft posts at all

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     {
       allMarkdownRemark(
         sort: { order: DESC, fields: [frontmatter___date] }
+        filter: { frontmatter: { draft: { ne: true } } }
         limit: 1000
       ) {
         edges {
