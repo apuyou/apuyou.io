6865f168e044d00061b0c0429c2e50e961deb467
Arthur Puyou <arthur@puyou.fr>
2021-04-10 21:18:37 +0200

    Load SVG directly

--- src/components/common/SEO/index.jsx
+++ src/components/common/SEO/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import Thumbnail from 'Static/thumbnail/thumbnail.png';
 import {
   url,
   description as defaultDescription,
@@ -15,7 +14,7 @@ export const SEO = ({
   description = defaultDescription,
   location = '',
 }) => {
-  const structuredDataOrganization = `{ 
+  const structuredDataOrganization = `{
 		"@context": "http://schema.org",
 		"@type": "Organization",
 		"legalName": "${legalName}",
@@ -35,20 +34,20 @@ export const SEO = ({
   return (
     <Helmet>
       <meta name="description" content={description} />
-      <meta name="image" content={Thumbnail} />
+      <meta name="image" content="/thumbnail.png" />
 
       <meta property="og:url" content={`${url}${location}`} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={Thumbnail} />
+      <meta property="og:image" content="/thumbnail.png" />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:site" content={twitterUsername} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image:src" content={Thumbnail} />
+      <meta name="twitter:image:src" content="/thumbnail.png" />
       <script type="application/ld+json">{structuredDataOrganization}</script>
       <title>{title}</title>
       <html lang="en" dir="ltr" />
