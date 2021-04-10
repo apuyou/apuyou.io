43fab07c4090c3d11e465080894aa7fa3097f8bc
Arthur Puyou <arthur@puyou.fr>
2021-04-10 21:17:44 +0200

    Migrate more stuff to Next

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { v4 as uuid } from 'uuid';
+
 export default function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  // useEffect(() => {
+  //   let deviceId = localStorage.getItem('device-id');
+  //   if (!deviceId) {
+  //     deviceId = uuid();
+  //     localStorage.setItem('device-id', deviceId);
+  //   }
+  //   axios.post('/.netlify/functions/view', {
+  //     device_id: deviceId,
+  //     referrer: document.referrer,
+  //   });
+  // }, []);
+
+  return 'Hi';
 }
