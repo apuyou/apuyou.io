5fe0ae5e3bf9db40e786112a106d57681b63558c
Arthur Puyou <arthur@puyou.fr>
2021-04-11 23:25:44 +0200

    Add some theming

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,13 @@
+/** @jsxImportSource theme-ui */
+
 export default function Layout({ children }) {
   return (
-    <div>
+    <div
+      sx={theme => ({
+        maxWidth: theme.breakpoints[theme.breakpoints.length - 1],
+        marginX: 'auto',
+      })}
+    >
       <div>Header</div>
       {children}
     </div>
--- theme.js
+++ theme.js
@@ -1,18 +1,87 @@
 export default {
+  breakpoints: ['40em', '56em'],
   fonts: {
-    body: 'system-ui, sans-serif',
-    heading: '"Avenir Next", sans-serif',
+    body: '"Crimson Text", serif',
+    heading: 'Rosario, serif',
     monospace: 'Menlo, monospace',
+  },
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
   },
   colors: {
     text: '#000',
     background: '#fff',
-    primary: '#33e',
+    primary: '#07c',
+    modes: {
+      dark: {
+        text: '#fff',
+        background: '#000',
+        primary: '#0cf',
+      },
+    },
+  },
+  text: {
+    heading: {
+      fontFamily: 'heading',
+      lineHeight: 'heading',
+      fontWeight: 'heading',
+    },
   },
   styles: {
     root: {
       fontFamily: 'body',
+      lineHeight: 'body',
       fontWeight: 'body',
+    },
+    h1: {
+      variant: 'text.heading',
+      fontSize: 5,
+    },
+    h2: {
+      variant: 'text.heading',
+      fontSize: 4,
+    },
+    h3: {
+      variant: 'text.heading',
+      fontSize: 3,
+    },
+    h4: {
+      variant: 'text.heading',
+      fontSize: 2,
+    },
+    h5: {
+      variant: 'text.heading',
+      fontSize: 1,
+    },
+    h6: {
+      variant: 'text.heading',
+      fontSize: 0,
+    },
+    pre: {
+      fontFamily: 'monospace',
+      overflowX: 'auto',
+      code: {
+        color: 'inherit',
+      },
+    },
+    code: {
+      fontFamily: 'monospace',
+      fontSize: 'inherit',
+    },
+    table: {
+      width: '100%',
+      borderCollapse: 'separate',
+      borderSpacing: 0,
+    },
+    th: {
+      textAlign: 'left',
+      borderBottomStyle: 'solid',
+    },
+    td: {
+      textAlign: 'left',
+      borderBottomStyle: 'solid',
     },
   },
 };
