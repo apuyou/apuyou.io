630e6f4ee6d92d773357529c4c179c3185b3ba6f
Arthur Puyou <arthur@puyou.fr>
2021-08-27 23:22:09 +0200

    Improve skills

--- components/skills.js
+++ components/skills.js
@@ -4,14 +4,16 @@ export default function Skills() {
   return (
     <>
       <h2 id="skills">Skills</h2>
-      <h3>Software Engineering</h3>
-      <p>
+      <h3 sx={{ marginBottom: 0 }}>Software Engineering</h3>
+      <p sx={{ marginTop: 0 }}>
         Full-stack software engineering and cloud architecture.
         <br />
         Python (Django). React. Docker. Serveless.
       </p>
-      <h3>Product Management</h3>
-      <p>From conception to driving a development team.</p>
+      <h3 sx={{ marginBottom: 0 }}>Product Management</h3>
+      <p sx={{ marginTop: 0 }}>
+        From conception to driving a development team.
+      </p>
     </>
   );
 }
